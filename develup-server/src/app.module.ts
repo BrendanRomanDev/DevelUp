873469f22a4bbef375089765b91a8fe334a29596
BrendanRomanDev <roman.bp15@gmail.com>
2023-04-01 16:06:00 -0400

    Postgres, GQL, Prisma added

--- develup-server/src/app.module.ts
+++ develup-server/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
     }),
+    CategoryModule,
   ],
 })
 export class AppModule {}
